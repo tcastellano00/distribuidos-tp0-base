2764e011f5c0612664be056cf1d0306ddaa71692
Tomas Castellano <tcastellano@fi.uba.ar>
2024-09-04 02:25:37 -0300

    add message and protocol in clien

---
+++ client/communication/message.go
@@ -0,0 +1,67 @@
+package communication
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ClientMessage struct {
+	agency    string
+	firstName string
+	lastName  string
+	document  string
+	birthdate string
+	number    string
+}
+
+type ServerMessage struct {
+	success bool
+	msg     string
+}
+
+func CreateClientMessage(
+	agency string, firstName string, lastName string, document string,
+	birthdate string, number string) *ClientMessage {
+
+	message := &ClientMessage{
+		agency:    agency,
+		firstName: firstName,
+		lastName:  lastName,
+		document:  document,
+		birthdate: birthdate,
+		number:    number,
+	}
+
+	return message
+}
+
+func CreateServerMessage(
+	success bool, msg string) *ServerMessage {
+
+	message := &ServerMessage{
+		success: success,
+		msg:     msg,
+	}
+
+	return message
+}
+
+func (msg *ClientMessage) Serialize() string {
+	return fmt.Sprintf("%s|%s|%s|%s|%s|%s\n",
+		msg.agency,
+		msg.firstName,
+		msg.lastName,
+		msg.document,
+		msg.birthdate,
+		msg.number,
+	)
+}
+
+func DeserializeServerMessage(serverMessage string) *ServerMessage {
+	messageParts := strings.Split(strings.TrimSpace(serverMessage), "|")
+
+	return CreateServerMessage(
+		messageParts[0] == "OK",
+		messageParts[1],
+	)
+}
---
+++ client/communication/protocol.go
@@ -0,0 +1,65 @@
+package communication
+
+import (
+	"net"
+	"strings"
+)
+
+const (
+	RECEIVE_SIZE_BYTES int = 1024
+)
+
+type Protocol struct {
+	socket net.Conn
+}
+
+func CreateConnection(serverAddress string) (*Protocol, error) {
+	conn, err := net.Dial("tcp", serverAddress)
+
+	if err != nil {
+		return nil, err
+	}
+
+	protocol := &Protocol{
+		socket: conn,
+	}
+
+	return protocol, nil
+}
+
+func (prot *Protocol) Send(message ClientMessage) error {
+	data := []byte(message.Serialize())
+
+	sentBytes := 0
+	totalBytesToSend := len(data)
+
+	for sentBytes < totalBytesToSend {
+		bytes, err := prot.socket.Write(data[sentBytes:])
+		if err != nil {
+			return err
+		}
+		sentBytes += bytes
+	}
+
+	return nil
+}
+
+func (prot *Protocol) Receive() (*ServerMessage, error) {
+	buffer := make([]byte, RECEIVE_SIZE_BYTES)
+	var message strings.Builder
+
+	for {
+		bytes, err := prot.socket.Read(buffer)
+		if err != nil {
+			return CreateServerMessage(false, "unexpected error"), err
+		}
+
+		message.Write(buffer[:bytes])
+
+		msg := message.String()
+
+		if strings.Contains(msg, "\n") {
+			return DeserializeServerMessage(msg), nil
+		}
+	}
+}
